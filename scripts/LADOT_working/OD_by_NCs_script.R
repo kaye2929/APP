8a5caf5a81b9f5168d8be4b1398b6829540c21fa
abecheung <110483064+abecheung@users.noreply.github.com>
2023-01-04 15:28:19 -0800

    1/4/2023 edited data cleaning script

--- scripts/LADOT_working/OD_by_NCs_script.R
+++ scripts/LADOT_working/OD_by_NCs_script.R
@@ -1,8 +1,9 @@
 ############### Script for Cleaning LADOT Data - OD by NCs ################
 
 # Set Up ########
-## set working directory
-setwd("C:/Project/APP/APP/data/LADOT/CPRA #22-10589 Data") 
+## set working directory to file location of this script
+setwd("C:/Project/APP/APP/scripts/LADOT_working")
+
 
 ## load packages
 pkgs <- c("tidyverse", "readxl", "lubridate")
@@ -12,25 +13,42 @@ rm(pkgs)
 
 # Cleaning ########
 years <- c("2019","2020","2021","2022") # the four years of the different workbooks
-output <- NULL
+my_output <- NULL
 
 for (yr in years) {
-  # obtain specific year-workbook and its sheet names
-  wkb_name <- paste(yr, "Neighborhood Council Trip Matrix.xlsx")
-  sheet_names <- excel_sheets(wkb_name)
+  # create path to the workbook with a specific year
+  path_name <- paste("../../data/LADOT/CPRA #22-10589 Data/", yr, " Neighborhood Council Trip Matrix.xlsx", sep = "")  
+  
+  # obtain all the sheet names within the specific workbook
+  sheet_names <- excel_sheets(path_name)
   
   # read, clean, and append all the sheets from the specific workbook
   for (one_month in sheet_names) {
-    one_sheet <- read_excel(wkb_name, sheet = one_month) %>% 
+    one_sheet <- read_excel(path_name, sheet = one_month) %>% 
       rename("Origin NC" = "Origin\\Destination") %>% 
-      mutate_at(vars(-c("Origin NC")), as.numeric) %>%
       pivot_longer(cols = -c("Origin NC"), names_to = "Destination NC", values_to = "Trips") %>% 
       mutate(Month = mdy(paste(one_month,"1",yr)))
     
-    output <- bind_rows(output, one_sheet)
+    my_output <- bind_rows(my_output, one_sheet)
   }
-  rm(one_sheet,yr,one_month,wkb_name,sheet_names)
+  
+  rm(one_sheet,yr,one_month,path_name,sheet_names)
 }
+
+
+# Add Geos ########
+library(sf)
+nc_geos = read_sf("../../data/geos/Neighborhood Councils (Certified)/geo_export_71f6a568-febe-403c-898f-4ce9286a58a2.shp")
+
+library(jsonlite)
+lanc_geos = fromJSON(txt = "https://maps.lacity.org/lahub/rest/services/Boundaries/MapServer/18/query?outFields=*&where=1%3D1&f=geojson")
+lanc_df = lanc_geos$features$properties
+
+sort(lanc_df$NC_ID) %in% sort(nc_geos$nc_id) # the NC_IDs match, but the names have changed. make sure we do the joins by the IDs not the names.
+
+library(geojsonio)
+spdf <- read_sf("https://maps.lacity.org/lahub/rest/services/Boundaries/MapServer/18/query?outFields=*&where=1%3D1&f=geojson")
+st_write(spdf, "is_this_.geojson")
 
 # Check Work ########
 
