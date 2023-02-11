1c48cc6058df931783bdd58e9b2782301e374d99
kaye2929 <karen80186@gmail.com>
2023-02-11 12:46:53 -0800

    02/11/2023 remove NAs

--- scripts/vehicle_deployment_cleaning.R
+++ scripts/vehicle_deployment_cleaning.R
@@ -14,7 +14,7 @@ ref <- st_read(file.path(file_path, 'NC_OldtoNew_Ref.geojson'))
 
 ## loop 
 years <- c("2019","2020","2021","2022")
-ref_sub <- ref %>% select(old_nc_name, new_nc_name, nc_id)
+ref_sub <- ref %>% select(data_nc_name, new_nc_name, nc_id)
 data_output <- NULL
 data_output2 <- NULL
 
@@ -22,33 +22,36 @@ for (yr in years) {
   
   # read sheets(pivot long)
   vd <- read_excel(path = sheet_path, sheet = paste('Deployment', yr, sep = " "), skip = 1) %>% 
-    pivot_longer(cols = -c('Neighborhood Council District'), names_to = 'month', values_to = 'Avg deployment') 
+    pivot_longer(cols = -c('Neighborhood Council District'), names_to = 'month', values_to = 'Avg_deployment') 
 
   # output of pivot long format
   data_output <- vd %>% 
     mutate(Month = mdy(paste(month,"1",yr))) %>% 
     
     # append data
-    bind_rows(data_output)
+    bind_rows(data_output) %>% 
+    
+    # remove NAs
+    filter(!is.na(Avg_deployment))
   
   # output of pivot wide format
   data_output2 <- data_output %>% 
     mutate(year = year(Month),
            month_year = paste(month, year, sep = '_')) %>% 
-    select('Neighborhood Council District', 'Avg deployment', 'month_year') %>% 
+    select('Neighborhood Council District', 'Avg_deployment', 'month_year') %>% 
     pivot_wider(names_from = month_year, 
-                values_from = 'Avg deployment')
+                values_from = 'Avg_deployment')
     
   }
 
 
 ## arrange data
 data_output <- data_output %>% 
-  select('Neighborhood Council District', 'Month', 'Avg deployment')
+  select('Neighborhood Council District', 'Month', 'Avg_deployment') 
 
 # add new nc name and nc id
-data_output <- ref_sub %>% left_join(data_output, by = c('old_nc_name' = 'Neighborhood Council District'))
-data_output2 <- ref_sub %>% left_join(data_output2, by = c('old_nc_name' = 'Neighborhood Council District'))
+data_output <- ref_sub %>% left_join(data_output, by = c('data_nc_name' = 'Neighborhood Council District'))
+data_output2 <- ref_sub %>% left_join(data_output2, by = c('data_nc_name' = 'Neighborhood Council District'))
 
 ## Export data (csv and geojson)
 # geojson
